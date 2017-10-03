f0a99070f2d0f04f44a0bfc4e6c3cc091051d740
samuelrayseldi <32404233+samuelrayseldi@users.noreply.github.com>
2017-10-03 13:55:23 -0700

    Update AggressiveDriver.java

--- java/src/AggressiveDriver.java
+++ java/src/AggressiveDriver.java
@@ -5,8 +5,25 @@ public class AggressiveDriver extends Driver {
     }
 
     @Override
-    public boolean stop_for_refueling(Car car, Environment env) {
-        return false;
+    public double throttle_action(Car car, Environment env) {
+
+        if(car.current_speed<car.max_speed)
+            return car.max_acceleration;
+        else
+            return car.max_acceleration;
     }
 
+    @Override
+    public boolean stop_for_refueling(Car car, Environment env) {
+
+        double next_station = env.get_next_station_following(car.position);
+        double stop_at = car.position + car.stopping_distance();
+
+        if( stop_at<next_station )
+            return true;
+        else
+            return false;
+
+
+    }
 }
